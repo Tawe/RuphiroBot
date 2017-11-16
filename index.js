f86cd683ebec72782fe749f76f311104b764d8ef
John Munn <jmunn@HXLM15-JMUNN.local>
2017-11-15 21:24:33 -0400

    Added back in bot.js

--- index.js
+++ index.js
@@ -1,55 +1,28 @@
 const http = require('http');
 const express = require('express')
 const app = express();
-const director = require('director');
+// const director = require('director');
 const bot = require('./bot.js');
 
-const router = new director.http.Router({
-  '/' : {
-    post: bot.respond,
-    get: ping
-  }
-});
+app.use((request, response, next) => {
+  console.log(request.headers)
+  next()
+})
+
+app.use((request, response, next) => {
+  request.chance = Math.random()
+  next()
+})
 
 app.get('/', (req, res) => {
   res.send('Hello World')
-  req.chunks = [];
-      req.on('data', function (chunk) {
-      req.chunks.push(chunk.toString());
-  });
   bot.respond(req, res);
-  // router.dispatch(req, res, function(err) {
-  //   res.writeHead(err.status, {"Content-Type": "text/plain"});
-  //   res.end(err.message);
-  // });
-})
+
+ })
 
 app.listen(5000, () => {
   console.log('listening on 3000')
 })
-
-// const requestHandler = (req, res) => {
-//   console.log(req.url)
-//   res.end('Hello Node.js Server!')
-//   req.chunks = [];
-//   req.on('data', function (chunk) {
-//     req.chunks.push(chunk.toString());
-//   });
-
-
-// }
-
-// const server = http.createServer(requestHandler)
-
-// const port = 5000;
-// server.listen(port, (err) => {
-//   if (err) {
-//     return console.log('something bad happened', err)
-//   }
-
-//   console.log(`server is listening on ${port}`)
-// })
-
 
 function ping() {
   this.res.writeHead(200);
