b29fe5fd332459d03e61c430cfb2ee233b595a84
John Munn <jmunn@HXLM15-JMUNN.local>
2017-11-15 21:23:19 -0400

    Added Express

--- bot.js
+++ bot.js
@@ -1,18 +1,18 @@
 var HTTPS = require('https');
 var botID = process.env.BOT_ID;
 
-function respond() {
-  var request = JSON.parse(this.req.chunks[0]),
+function respond(req, res) {
+  var request = JSON.parse(req.chunks[0]),
       botRegex = /^\/ruphiro$/;
 
   if(request.text && botRegex.test(request.text)) {
-    this.res.writeHead(200);
+    res.writeHead(200);
     postMessage();
-    this.res.end();
+    res.end();
   } else {
     console.log("don't care");
-    this.res.writeHead(200);
-    this.res.end();
+    res.writeHead(200);
+    res.end();
   }
 }
 
--- index.js
+++ index.js
@@ -1,30 +1,55 @@
-var http, director, cool, bot, router, server, port;
+const http = require('http');
+const express = require('express')
+const app = express();
+const director = require('director');
+const bot = require('./bot.js');
 
-http        = require('http');
-director    = require('director');
-bot         = require('./bot.js');
-
-router = new director.http.Router({
+const router = new director.http.Router({
   '/' : {
     post: bot.respond,
     get: ping
   }
 });
 
-server = http.createServer(function (req, res) {
+app.get('/', (req, res) => {
+  res.send('Hello World')
   req.chunks = [];
-  req.on('data', function (chunk) {
-    req.chunks.push(chunk.toString());
+      req.on('data', function (chunk) {
+      req.chunks.push(chunk.toString());
   });
+  bot.respond(req, res);
+  // router.dispatch(req, res, function(err) {
+  //   res.writeHead(err.status, {"Content-Type": "text/plain"});
+  //   res.end(err.message);
+  // });
+})
 
-  router.dispatch(req, res, function(err) {
-    res.writeHead(err.status, {"Content-Type": "text/plain"});
-    res.end(err.message);
-  });
-});
+app.listen(5000, () => {
+  console.log('listening on 3000')
+})
 
-port = Number(process.env.PORT || 5000);
-server.listen(port);
+// const requestHandler = (req, res) => {
+//   console.log(req.url)
+//   res.end('Hello Node.js Server!')
+//   req.chunks = [];
+//   req.on('data', function (chunk) {
+//     req.chunks.push(chunk.toString());
+//   });
+
+
+// }
+
+// const server = http.createServer(requestHandler)
+
+// const port = 5000;
+// server.listen(port, (err) => {
+//   if (err) {
+//     return console.log('something bad happened', err)
+//   }
+
+//   console.log(`server is listening on ${port}`)
+// })
+
 
 function ping() {
   this.res.writeHead(200);
