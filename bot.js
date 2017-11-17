a4e4aac33278dad12335fd586a02166c0c2ee842
John Munn <jmunn@HXLM15-JMUNN.local>
2017-11-17 00:46:54 -0400

    Reverting bot.js

--- bot.js
+++ bot.js
@@ -1,55 +1,32 @@
-var HTTPS = require('https');
-var botID = process.env.BOT_ID;
+var http, director, bot, router, server, port;
 
-function respond(req, res) {
-  var request = JSON.parse(req.chunks[0]),
-      botRegex = /^\/ruphiro$/;
+http        = require('http');
+director    = require('director');
+bot         = require('./bot.js');
 
-  if(request.text && botRegex.test(request.text)) {
-    res.writeHead(200);
-    postMessage();
-    res.end();
-  } else {
-    console.log("don't care");
-    res.writeHead(200);
-    res.end();
+router = new director.http.Router({
+  '/' : {
+    post: bot.respond,
+    get: ping
   }
+});
+
+server = http.createServer(function (req, res) {
+  req.chunks = [];
+  req.on('data', function (chunk) {
+    req.chunks.push(chunk.toString());
+  });
+
+  router.dispatch(req, res, function(err) {
+    res.writeHead(err.status, {"Content-Type": "text/plain"});
+    res.end(err.message);
+  });
+});
+
+port = Number(process.env.PORT || 5000);
+server.listen(port);
+
+function ping() {
+  this.res.writeHead(200);
+  this.res.end("Hey, I'm Cool Guy.");
 }
-
-function postMessage() {
-  var botResponse = 'Yes Sir!'
-  var options, body, botReq;
-
-
-  options = {
-    hostname: 'api.groupme.com',
-    path: '/v3/bots/post',
-    method: 'POST'
-  };
-
-  body = {
-    "bot_id" : botID,
-    "text" : botResponse
-  };
-
-  console.log('sending ' + botResponse + ' to ' + botID);
-
-  botReq = HTTPS.request(options, function(res) {
-      if(res.statusCode == 202) {
-        //neat
-      } else {
-        console.log('rejecting bad status code ' + res.statusCode);
-      }
-  });
-
-  botReq.on('error', function(err) {
-    console.log('error posting message '  + JSON.stringify(err));
-  });
-  botReq.on('timeout', function(err) {
-    console.log('timeout posting message '  + JSON.stringify(err));
-  });
-  botReq.end(JSON.stringify(body));
-}
-
-
-exports.respond = respond;
