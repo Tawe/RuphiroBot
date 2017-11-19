cdf4c76ee6a79bb7688f230c80defa9ea57a43e5
John Munn <jmunn@HXLM15-JMUNN.local>
2017-11-19 16:39:59 -0400

    More command debugging

--- bot.js
+++ bot.js
@@ -17,25 +17,24 @@ HTTPS.get(url, function(res){
 });
 
 function respond() {
-  var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\/ruphiro\s/g;
+  var request = JSON.parse(this.req.chunks[0])
 
-  if(request.text && request.text.startsWith("/ruphiro")) {
+  // if(request.text && request.text.startsWith("/ruphiro")) {
     this.res.writeHead(200);
-    postMessage();
+    postMessage(request.text);
     this.res.end();
-  } else {
-    console.log("don't care");
-    this.res.writeHead(200);
-    this.res.end();
-  }
+  // } else {
+  //   console.log("don't care");
+  //   this.res.writeHead(200);
+  //   this.res.end();
+  // }
 }
 
-function postMessage() {
+function postMessage(copy)) {
   var botResponse, options, body, botReq; 
   var data = JSON.parse(campaignData);
-  botResponse = 'Each party member currently has ' + data.dragonsDemand.groupExp + ' Each! ';
-
+  // botResponse = 'Each party member currently has ' + data.dragonsDemand.groupExp + ' Each! ';
+  botResponse = copy;
   options = {
     hostname: 'api.groupme.com',
     path: '/v3/bots/post',
@@ -67,4 +66,13 @@ function postMessage() {
 }
 
 
+function mock(mockData){
+  if(mockData.text && mockData.text.startsWith("/ruphiro")) {
+    var data = JSON.parse(campaignData);
+    botResponse = 'Each party member currently has ' + data.dragonsDemand.groupExp + ' Each! ';
+    return botResponse;
+  } 
+}
+
 exports.respond = respond;
+exports.mock = mock;
