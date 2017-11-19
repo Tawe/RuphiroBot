3cf24de6b1c395bb5d98beba16153ab990db7c6f
John Munn <jmunn@HXLM15-JMUNN.local>
2017-11-19 13:36:56 -0400

    removing unsed modules

--- bot.js
+++ bot.js
@@ -1,5 +1,4 @@
 var HTTPS = require('https');
-var cool = require('cool-ascii-faces');
 
 var botID = process.env.BOT_ID;
 
@@ -21,7 +20,7 @@ function respond() {
 function postMessage() {
   var botResponse, options, body, botReq;
 
-  botResponse = cool();
+  botResponse = 'Yes?';
 
   options = {
     hostname: 'api.groupme.com',
