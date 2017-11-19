70bf47a7278971988896eee3b3388a053b95b000
John Munn <jmunn@HXLM15-JMUNN.local>
2017-11-19 16:09:12 -0400

    Redoing data call

--- bot.js
+++ bot.js
@@ -1,6 +1,21 @@
 var HTTPS = require('https');
-
 var botID = process.env.BOT_ID;
+
+var url = 'https://raw.githubusercontent.com/Tawe/RuphiroBot/master/data/data.json';
+HTTPS.get(url, function(res){
+    var body = '';
+
+    res.on('data', function(chunk){
+        body += chunk;
+    });
+
+    res.on('end', function(){
+        var fbResponse = JSON.parse(body);
+        console.log("Got a response: ", fbResponse.picture);
+    });
+}).on('error', function(e){
+      console.log("Got an error: ", e);
+});
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
