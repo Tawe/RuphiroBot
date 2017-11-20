1df8e925a8e3c1238fe31cb59fb0c96aaf3a5b91
John Munn <jmunn@HXLM15-JMUNN.local>
2017-11-19 22:07:45 -0400

    better output

--- bot.js
+++ bot.js
@@ -34,7 +34,7 @@ function respond() {
 function postMessage() {
   var botResponse, options, body, botReq;
   var data = JSON.parse(campaignData)  
-  botResponse = 'Sigh '+ data.dragonsDemand;
+  botResponse = 'Sigh '+ data.dragonsDemand.groupExp;
 
   options = {
     hostname: 'api.groupme.com',
