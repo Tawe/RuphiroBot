dd81c03fd061fff2980e250351b78e82e8a5703d
John Munn <jmunn@HXLM15-JMUNN.local>
2017-11-23 09:15:41 -0400

    Fixing level count

--- bot.js
+++ bot.js
@@ -1,29 +1,30 @@
-var HTTPS = require('https');
+var HTTPS = require("https");
 var botID = process.env.BOT_ID;
 var campaignData;
 var strArr = [];
-var url = 'https://raw.githubusercontent.com/Tawe/RuphiroBot/master/data/data.json';
+var url =
+  "https://raw.githubusercontent.com/Tawe/RuphiroBot/master/data/data.json";
 
-HTTPS.get(url, function(res){
-    var body = '';
+HTTPS.get(url, function(res) {
+  var body = "";
 
-    res.on('data', function(chunk){
-        body += chunk;
-    });
+  res.on("data", function(chunk) {
+    body += chunk;
+  });
 
-    res.on('end', function(){
-        campaignData = body;
-    });
-}).on('error', function(e){
-      console.log("Got an error: ", e);
+  res.on("end", function() {
+    campaignData = body;
+  });
+}).on("error", function(e) {
+  console.log("Got an error: ", e);
 });
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\/ruphiro\s/;
+    botRegex = /^\/ruphiro\s/;
 
-  if(request.text && botRegex.test(request.text)) {
-    strArr =request.text.split(' ');
+  if (request.text && botRegex.test(request.text)) {
+    strArr = request.text.split(" ");
     this.res.writeHead(200);
     postMessage(strArr[1]);
     this.res.end();
@@ -36,67 +37,79 @@ function respond() {
 
 function postMessage(copy) {
   var botResponse, options, body, botReq;
-  // var data = JSON.parse(campaignData)  
-  if(copy == 'exp'){
-    botResponse = handleExp()
-  } else if(copy == 'location'){
+  if (copy == "exp") {
+    botResponse = handleExp();
+  } else if (copy == "location") {
     botResponse = handleLocation();
+  } else if (copy == "plus") {
+    botResponse = handlePlus();
   }
-  // botResponse = 'Sigh '+ data.dragonsDemand.groupExp +' '+copy;
 
   options = {
-    hostname: 'api.groupme.com',
-    path: '/v3/bots/post',
-    method: 'POST'
+    hostname: "api.groupme.com",
+    path: "/v3/bots/post",
+    method: "POST"
   };
 
   body = {
-    "bot_id" : botID,
-    "text" : botResponse
+    bot_id: botID,
+    text: botResponse
   };
 
-  console.log('sending ' + botResponse + ' to ' + botID);
+  console.log("sending " + botResponse + " to " + botID);
 
   botReq = HTTPS.request(options, function(res) {
-      if(res.statusCode == 202) {
-        //neat
-      } else {
-        console.log('rejecting bad status code ' + res.statusCode);
-      }
+    if (res.statusCode == 202) {
+      //neat
+    } else {
+      console.log("rejecting bad status code " + res.statusCode);
+    }
   });
 
-  botReq.on('error', function(err) {
-    console.log('error posting message '  + JSON.stringify(err));
+  botReq.on("error", function(err) {
+    console.log("error posting message " + JSON.stringify(err));
   });
-  botReq.on('timeout', function(err) {
-    console.log('timeout posting message '  + JSON.stringify(err));
+  botReq.on("timeout", function(err) {
+    console.log("timeout posting message " + JSON.stringify(err));
   });
   botReq.end(JSON.stringify(body));
 }
 
-function handleExp(){
+function handleExp() {
   var data = JSON.parse(campaignData);
   var currentExp = data.dragonsDemand.groupExp;
   var levelExp = data.dragonsDemand.expPath.medium;
   var currentLevel = 0;
   var maxExp = 0;
 
-  for(var i = 0; i<levelExp.length; i++){
-    if(levelExp[i] > currentExp){
-        currentLevel = i;
-        maxExp = levelExp[i];
-        break;
+  for (var i = 0; i < levelExp.length; i++) {
+    if (levelExp[i] > currentExp) {
+      currentLevel = i + 1;
+      maxExp = levelExp[i];
+      break;
     }
   }
 
   var expDiff = maxExp - currentExp;
-
-  return "The party is currently level "+ currentLevel+", and each party memeber has "+currentExp +"xp. Which puts them "+expDiff+"xp away from their next level!";
+  return (
+    "The party is currently level " +
+    currentLevel +
+    ", and each party memeber has " +
+    currentExp +
+    "xp. Which puts them " +
+    expDiff +
+    "xp away from their next level!"
+  );
 }
 
-function handleLocation(){
+function handleLocation() {
   var data = JSON.parse(campaignData);
   var location = data.dragonsDemand.location;
-  return 'The party is currently located in ' + location;
+  return "The party is currently located in " + location;
 }
+
+function handlePlus() {
+  return "Ummm what should I sing ?";
+}
+
 exports.respond = respond;
